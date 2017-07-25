34d06900055f2a2324ecc953b89e04e49cbdf724
Rosi Lutfiana <rosilutfi@gmail.com>
2017-07-25 18:55:27 +0700

    --

---
+++ js/agency.js
@@ -0,0 +1,52 @@
+// Agency Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+    $(window).on("load resize scroll",function(e){ 
+        if ($(window).width() > 480) {
+           $('.navtop').append($('.navbar-collapse'));
+           $('.navbar-collapse').removeClass('navbar-onclick');
+        }
+        else {
+           $('.navcontainer').append($('.navbar-collapse'));
+           $('.navbar-collapse').addClass('navbar-onclick');
+        }
+    })
+
+
+    $('.navbar-toggle').click(function(){
+        $('.navbar-custom').toggleClass('navbar-onclick');
+    })
+
+    var h = window.innerHeight;
+    $('header').css({'height': h, 'width': 'auto'});
+
+})(jQuery); // End of use strict
